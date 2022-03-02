7ffc16eec4f80d72167b874cc8b66a648e955a54
Bernhard Leichtle <42609425+Bernhard-L@users.noreply.github.com>
2022-03-02 22:55:14 +0100

    updated fibunacci

--- 01_simple_number_sequence_asynchron_generator.hpp
+++ 01_simple_number_sequence_asynchron_generator.hpp
@@ -1,46 +1,42 @@
-#include <iostream>
+#include "random_number.hpp"
 
 #include <cppcoro/async_generator.hpp>
 #include <cppcoro/sync_wait.hpp>
 #include <cppcoro/task.hpp>
+#include <iostream>
 
-#include "random_number.hpp"
+namespace simple_number_sequence_asynchron_generator
+{
+	cppcoro::async_generator<unsigned int> next_fibunacci(unsigned int maxValue)
+	{
+		unsigned int x = 1;
+		unsigned int y = 1;
+		while (y <= maxValue)
+		{
+			co_yield y;
+			x += y;
+			std::swap(x, y);
+		}
+	}
 
-namespace simple_number_sequence_asynchron_generator {
-    cppcoro::async_generator<unsigned int> next_fibunacci(unsigned int stopAfter)
-    {
-        unsigned int x = 0;
-        unsigned int y = 1;
-        while (y < stopAfter)
-        {
-            x += y;
-            std::swap(x, y);
-            co_yield y;
-        }
-    }
+	cppcoro::task<> consume_items(unsigned int maxValue) noexcept
+	{
+		std::cout << "fibunacci until max" << maxValue << std::endl;
 
-    cppcoro::task<> consume_items(unsigned int stopAfter) noexcept
-    {
-        std::cout << "stopping after the next fibunacci after " << stopAfter << std::endl;
+		for
+			co_await(auto const& s : next_fibunacci(maxValue)) { std::cout << "consumed item " << s << std::endl; }
+		std::cout << std::endl;
+	}
 
-        for
-            co_await(auto const& s
-                     : next_fibunacci(stopAfter))
-            {
-                std::cout << "consumed item " << s << std::endl;
-            }
-        std::cout << std::endl;
-    }
+	void run()
+	{
+		std::cout << "simple_number_sequence_asynchron_generator" << std::endl << std::endl;
 
-    void run()
-    {
-        std::cout << "simple_number_sequence_asynchron_generator" << std::endl << std::endl;
+		cppcoro::sync_wait(consume_items(3));
+		cppcoro::sync_wait(consume_items(4));
+		cppcoro::sync_wait(consume_items(5));
+		cppcoro::sync_wait(consume_items(25));
 
-        cppcoro::sync_wait(consume_items(3));
-        cppcoro::sync_wait(consume_items(4));
-        cppcoro::sync_wait(consume_items(5));
-        cppcoro::sync_wait(consume_items(25));
-
-        std::cout << std::endl;
-    }
-}
+		std::cout << std::endl;
+	}
+} // namespace simple_number_sequence_asynchron_generator
--- 04_finite_number_sequence_stacked_tasks_asynchron_generator.hpp
+++ 04_finite_number_sequence_stacked_tasks_asynchron_generator.hpp
@@ -1,52 +1,50 @@
-#include <iostream>
+#include "random_number.hpp"
 
 #include <cppcoro/async_generator.hpp>
 #include <cppcoro/recursive_generator.hpp>
 #include <cppcoro/sync_wait.hpp>
 #include <cppcoro/task.hpp>
+#include <iostream>
 
-#include "random_number.hpp"
+namespace finite_number_sequence_stacked_tasks_asynchron_generator
+{
+	cppcoro::async_generator<unsigned int> next_fibunacci(unsigned int maxValue)
+	{
+		unsigned int x = 1;
+		unsigned int y = 1;
+		while (y <= maxValue)
+		{
+			co_yield y;
+			x += y;
+			std::swap(x, y);
+		}
+	}
 
-namespace finite_number_sequence_stacked_tasks_asynchron_generator {
-    cppcoro::async_generator<unsigned int> next_fibunacci(unsigned int stopAfter)
-    {
-        unsigned int x = 0;
-        unsigned int y = 1;
-        
-        while (y < stopAfter)
-        {
-            x += y;
-            std::swap(x, y);
-            co_yield y;
-        }
-    }
+	//	cppcoro::async_generator<unsigned int>& relaying_task(unsigned int maxValue) noexcept { return next_fibunacci(maxValue); }
+	//
+	//	cppcoro::task<unsigned int> relay_item(unsigned int maxValue) noexcept
+	//	{
+	//	  for
+	//		  co_await(const auto& s : relaying_task(stopAfter)) { co_return s; }
+	//	}
 
-  //  cppcoro::recursive_generator<unsigned int> relay_item(unsigned int stopAfter) noexcept
-  //  {
-  //      co_yield next_fibunacci(stopAfter);
-  //  }
-  //
-  //  cppcoro::task<> consume_items(unsigned int stopAfter) noexcept
-  //  {
-  //    std::cout << "stopping after the next fibunacci after " << stopAfter << std::endl;
-  //      for
-  //          co_await(const auto & s
-  //                   : relay_item(stopAfter))
-  //          {
-  //              std::cout << "consumed item " << s << std::endl;
-  //          }
-  //      std::cout << std::endl;
-  //  }
-  //
-    void run()
-    {
-        std::cout << "finite_number_sequence_stacked_tasks_asynchron_generator" << std::endl << std::endl;
-  //
-  //      cppcoro::sync_wait(consume_items(3));
-  //      cppcoro::sync_wait(consume_items(4));
-  //      cppcoro::sync_wait(consume_items(5));
-  //      cppcoro::sync_wait(consume_items(25));
-  //
-        std::cout << std::endl;
-    }
-}
+	cppcoro::task<> consume_items(unsigned int maxValue) noexcept
+	{
+		std::cout << "fibunacci until max" << maxValue << std::endl;
+	  for
+		  co_await(const auto& s : next_fibunacci(maxValue)) { std::cout << "consumed item " << s << std::endl; }
+	  std::cout << std::endl;
+	}
+
+	void run()
+	{
+		std::cout << "finite_number_sequence_stacked_tasks_asynchron_generator" << std::endl << std::endl;
+		//
+		//      cppcoro::sync_wait(consume_items(3));
+		//      cppcoro::sync_wait(consume_items(4));
+		//      cppcoro::sync_wait(consume_items(5));
+		//      cppcoro::sync_wait(consume_items(25));
+		//
+		std::cout << std::endl;
+	}
+} // namespace finite_number_sequence_stacked_tasks_asynchron_generator
